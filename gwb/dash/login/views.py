7d53a6abc34bce5ef0b5d0a030104d4e2c8f1dbc
andr231 <chinoxtrons7@gmail.com>
2019-09-09 20:10:01 -0500

    esta es la primer actualizacion de mi proyecto

---
+++ gwb/dash/login/views.py
@@ -0,0 +1,25 @@
+#from django.shortcuts import render
+#from django.shortcuts import render, redirect
+#from django.contrib.auth import authenticate, login as do_login, logout as do_logout
+#from login.forms import LoginForm, ProfileForm
+#from login.models import Profile
+#from django.contrib.auth import login
+#from login.forms import SignUpForm
+#codigo dacil
+from django.contrib.auth.models import User
+from  django.contrib.auth.forms import UserCreationForm
+from django.views.generic import CreateView
+from django.urls import reverse_lazy
+#from django.core.urlresolvers
+#from login.forms import Registro
+#from django.contrib.auth.views import LoginView, LogoutView
+
+
+# Create your views here.
+#codigo dacil
+class Reusuario(CreateView):
+    """docstring for Reusuario"""
+   # model = User
+    template_name = 'login/signup.html'
+    #form_class = RegistroF
+    #success_url = reverse_lazy('login:login')
---
+++ gwb/dash/producto/models.py
@@ -0,0 +1,42 @@
+from django.db import models
+# Create your models here.
+class Producto(models.Model):
+    nombre = models.CharField(max_length=300)
+    descripcion = models.CharField(max_length=300)
+    precio = models.IntegerField()
+
+    def __str__(self):
+        return "producto: {} precio: $ {}".format(self.nombre, self.precio)
+ 
+
+class Comentario(models.Model):
+    comentario = models.CharField(max_length=300)
+    usuario = models.CharField(max_length=300)
+    fecha = models.DateTimeField(auto_now_add=True)
+    producto = models.ForeignKey(Producto, null=True, blank=True, related_name="producto_comentarios",on_delete=models.CASCADE)
+
+    def __str__(self):
+        return "{} {}".format(self.comentario, self.producto)
+
+class ImagenesProducto(models.Model):
+    descripcion = models.CharField(max_length=300)
+    producto = models.ForeignKey(Producto, null=True, blank=True, related_name="producto_imagenes",on_delete=models.CASCADE)
+    imagen = models.ImageField(upload_to='static/imagenes/imagenes_producto')
+
+    def __str__(self):
+        return "{}".format(self.descripcion)
+
+
+class CarritoCompras(models.Model):
+    producto = models.ForeignKey(Producto, null=True, blank=True, related_name="producto_carrito",on_delete=models.CASCADE)
+    precio = models.IntegerField()
+    identificador = models.IntegerField(null=True)
+    direccion = models.CharField(max_length=300)
+    datos_payu = models.TextField()
+    comprado = models.BooleanField(default=False)
+    pendiente = models.BooleanField(default=False)
+    
+
+
+    def __str__(self):
+        return "{} {}".format(self.usuario, self.producto)
